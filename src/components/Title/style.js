695f772b88d73610cf36c7c85ab4e4e1486ce537
alissonhfp2003 <129643644+alissonhfp2003@users.noreply.github.com>
2024-04-14 22:50:33 -0300

    Title style.js

---
+++ src/components/Title/style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  boxImage: {
+    flex: 5,
+    justifyContent: 'flex-start', // Alinhar ao topo
+    alignItems: 'center',
+    paddingTop: 10, // Espaço superior para a imagem
+  },
+  Image: {
+    width: 200, // Largura da imagem
+    height: 60, // Altura da imagem
+    resizeMode: 'contain', // Ajuste da imagem
+  },
+  });
+  
+  export default styles;
+  
